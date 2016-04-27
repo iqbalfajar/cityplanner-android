7e4e65d45b51325a33ef855975199b41bcfe1954
Muhammad Reza Irvanda <muzavan@gmail.com>
2016-04-27 13:52:26 +0700

    Main Mockup Done.

--- app/src/main/java/com/muzavan/userpc/cityplanner/DetailActivity.java
+++ app/src/main/java/com/muzavan/userpc/cityplanner/DetailActivity.java
@@ -9,5 +9,6 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        getSupportActionBar().setTitle("Detail Zona");
     }
 }
