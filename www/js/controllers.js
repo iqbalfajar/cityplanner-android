12f1fff7e3c62295b8378876ba2d30c3fd162088
Iqbal Fajar <iqbalfajar@gmail.com>
2016-04-29 23:06:34 +0700

    city watch optimize show layer

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -622,7 +622,7 @@ angular.module('app.controllers', [])
     $scope.show.Pendidikan = true;
     $scope.show.Perdagangan_Jasa_Linear = false;
     $scope.show.Peribadatan = true;
-    $scope.show.Perumahan_Kepadatan_Tinggi = true;
+    $scope.show.Perumahan_Kepadatan_Tinggi = false;
     $scope.show.Pusat_Perdagangan_Jasa = true;
     // $scope.show.RTH_Taman_Kota = true;
     // $scope.show.RTNH = true;
@@ -635,7 +635,7 @@ angular.module('app.controllers', [])
       $scope.loadGeojson('Pendidikan','rgba(245,124,0,44)', $scope.show.Pendidikan);
       // $scope.loadGeojson('Perdagangan_Jasa_Linear','rgba(255,61,0,72)', $scope.show.Perdagangan_Jasa_Linear);
       $scope.loadGeojson('Peribadatan','rgba(255,195,0,13)', $scope.show.Peribadatan);
-      $scope.loadGeojson('Perumahan_Kepadatan_Tinggi','rgba(255,193,7,14)', $scope.show.Perumahan_Kepadatan_Tinggi);
+      // $scope.loadGeojson('Perumahan_Kepadatan_Tinggi','rgba(255,193,7,14)', $scope.show.Perumahan_Kepadatan_Tinggi);
       $scope.loadGeojson('Pusat_Perdagangan_Jasa','rgba(249,0,0,79)', $scope.show.Pusat_Perdagangan_Jasa);
       // $scope.loadGeojson('RTH_Taman_Kota','rgba(85,255,0,-71)', $scope.show.RTH_Taman_Kota);
       // $scope.loadGeojson('RTNH','rgba(85,255,0,-71)', $scope.show.RTNH);
