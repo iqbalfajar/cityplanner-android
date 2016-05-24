c1a67e3571f894fa2353f27a7716696fec18128b
Muhammad Reza Irvanda <muzavan@gmail.com>
2016-05-24 17:38:05 +0700

    Fixing Profile Page

---
+++ app/src/main/java/com/muzavan/userpc/cityplanner/ProfileFragment.java
@@ -0,0 +1,27 @@
+package com.muzavan.userpc.cityplanner;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by user pc on 5/24/2016.
+ */
+public class ProfileFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View out = inflater.inflate(R.layout.fragment_profile,container);
+
+        return out;
+    }
+}
